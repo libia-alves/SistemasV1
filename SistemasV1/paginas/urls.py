9ba5fa7a349b7c914b09822d15dad3974cf4421f
libia-alves <libia.canhete@gmail.com>
2024-02-26 21:24:07 -0400

    ajustando rotas

--- SistemasV1/paginas/urls.py
+++ SistemasV1/paginas/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home,name="home"),
+    path('', views.home,name="index"),
     path('login/', views.login,name="login"),
+    path('cardapio/', views.cardapio,name="cardapio"),
+    path('login/', views.cardapio,name="login"),
+    path('perfil/', views.cardapio,name="perfil")
     
 ]
--- SistemasV1/paginas/views.py
+++ SistemasV1/paginas/views.py
@@ -8,7 +8,13 @@ def home(request):
     return render(request, "paginas/index.html") 
     
 
-   
 def login(request):
     return render(request, "paginas/login.html") 
-       
+
+
+def cardapio(request):
+    return render(request, "paginas/cardapio.html")
+
+    
+def perfil(request):
+    return render(request, "paginas/perfil")
